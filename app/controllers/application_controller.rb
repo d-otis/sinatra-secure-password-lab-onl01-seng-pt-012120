3c9788f8e40f8155ae6477f8f0dcc8bf63fb3c7a
Dan Foley <dan@dan-foley.com>
2020-04-09 19:24:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     binding.pry
+    user = User.new(username: params[:username], password: params[:password], balance: 0.0)
   end
 
   get '/account' do
