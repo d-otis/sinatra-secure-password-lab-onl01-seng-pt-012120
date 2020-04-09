9f8b9594003ec8871a30a168111994009bbc1485
Dan Foley <dan@dan-foley.com>
2020-04-09 19:27:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    binding.pry
     user = User.new(username: params[:username], password: params[:password], balance: 0.0)
     user.save ? redirect('/login') : redirect('/failure')
   end
